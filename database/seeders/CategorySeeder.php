d49a541a55afd457abd6780567ca36448a839249
InstaCodex <kevinmitnick@gmail.com>
2026-02-05 14:33:37 +0700

    update seeder saja

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Category::create([
+            'name' => 'Web Desain',
+            'slug' => 'web-desain',
+        ]);
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-desain',
+        ]);
+        Category::create([
+            'name' => 'Web Three',
+            'slug' => 'web-three',
+        ]);
+    }
+}
